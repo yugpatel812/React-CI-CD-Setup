354e7b7204eaa59294f3030eebc1602a840110bd
yug <yug42286@gmail.com>
2025-07-26 22:03:54 +0530

    ci-cd

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { configDefaults } from 'vitest/config'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js', // Add this line if you have a setup file
+    exclude: [...configDefaults.exclude, 'e2e/*'], // Example of excluding certain files from tests
+  },
 })
